dcedd4582ed5e30f68f974ad979941cb5fa43572
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-12-25 12:51:31 +0530

    Working

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -1,61 +1,106 @@
 import axios from "axios"
+import { MDBBtn } from "mdbreact"
 import React, { useCallback, useState } from "react"
 import { useDropzone } from "react-dropzone"
 import Image from "../../image"
+// import "./style.sass"
 
 const IndexPage = () => {
-  const [image, setImage] = useState<any[]>()
-  const onDrop = useCallback(async acceptedFiles => {
-    const data = new FormData()
-    acceptedFiles.forEach(file => {
-      data.append("photos", file)
-    })
+  const [images, setImages] = useState<any[]>()
+  const [processedImages, setProcessedImages] = useState<any[]>()
+  const [quality, setQuality] = useState("0")
+  const [greyscale, setGreyscale] = useState(false)
 
-    const ServerURL = "https://image-processor-backend.herokuapp.com/"
-    // Do something with the files
+  const onDrop = useCallback(acceptedFiles => {
+    let imageList = []
+    if (images) {
+      imageList.push(...images, acceptedFiles)
+    } else {
+      imageList = acceptedFiles
+    }
+    setImages(imageList)
+  }, [])
+
+  async function convert() {
+    const form = new FormData()
+    images.forEach(file => {
+      form.append("photos", file)
+    })
+    if (greyscale) {
+      form.append("greyscale", "true")
+    } else {
+      form.append("greyscale", "")
+    }
+    form.append("quality", quality)
+
+    const ServerURL = "http://localhost:3000/"
+
     const response = await axios({
-      //   photos: acceptedFiles,
       method: "post",
       url: ServerURL,
       headers: { "Content-Type": "multipart/form-data" },
-      data: data,
+      data: form,
     })
-    setImage(response.data)
-    console.log(response.data)
-  }, [])
+
+    setProcessedImages(response.data)
+  }
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
-  if (image) {
-    return (
-      <>
-        <ul>
-          {image.map((iamged, index) => {
-            return <img key={index} src={iamged} height="100" />
-          })}
-        </ul>
-      </>
-    )
-  } else {
-    return (
-      <>
-        <div {...getRootProps()}>
-          <input {...getInputProps()} />
-          {isDragActive ? (
-            <div>
-              Drop the files here ...
-              <Image />
-            </div>
-          ) : (
-            <div>
-              Drag 'n' drop some files here, or click to select files
-              <Image />
-            </div>
-          )}
-        </div>
-        {JSON.stringify(image)}
-      </>
-    )
-  }
+
+  return (
+    <>
+      <div {...getRootProps()}>
+        <input {...getInputProps()} />
+        {isDragActive ? (
+          <div>
+            Drop the files here ...
+            <Image />
+          </div>
+        ) : (
+          <div>
+            Drag 'n' drop some files here, or click to select files
+            <Image />
+          </div>
+        )}
+      </div>
+      <div className="custom-control custom-switch">
+        <input
+          type="checkbox"
+          className="custom-control-input"
+          id="customSwitches"
+          checked={greyscale}
+          onChange={() => setGreyscale(!greyscale)}
+          readOnly
+        />
+        <label className="custom-control-label" htmlFor="customSwitches">
+          Toggle this switch element
+        </label>
+      </div>
+      <div className="my-5">
+        <label htmlFor="customRange1">Quality range</label>
+        <input
+          type="range"
+          min={0}
+          max={100}
+          value={quality}
+          onChange={({ currentTarget }) => setQuality(currentTarget.value)}
+          className="custom-range"
+          id="customRange1"
+        />
+      </div>
+      <MDBBtn onClick={() => convert()}>Convert</MDBBtn>
+      {processedImages ? (
+        <>
+          Preview -
+          <ul>
+            {processedImages.map((image, index) => {
+              return <img key={index} src={image} height="100" />
+            })}
+          </ul>
+        </>
+      ) : null}
+    </>
+  )
 }
 
 export default IndexPage
